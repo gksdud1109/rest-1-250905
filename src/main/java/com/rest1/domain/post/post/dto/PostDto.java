4c8c8aecb4d25d9d40d1a5436566f51f89ac8c7b
gksdud1109 <vectorh532@gmail.com>
2025-09-08 10:47:26 +0900

    refactor(PostDto): impl record for dto -> immutable

--- src/main/java/com/rest1/domain/post/post/dto/PostDto.java
+++ src/main/java/com/rest1/domain/post/post/dto/PostDto.java
@@ -1,24 +1,23 @@
 package com.rest1.domain.post.post.dto;
 
 import com.rest1.domain.post.post.entity.Post;
-import lombok.Getter;
 
 import java.time.LocalDateTime;
 
-@Getter
-public class PostDto {
-
-    private Long id;
-    private LocalDateTime createDate;
-    private LocalDateTime modifyDate;
-    private String subject;
-    private String body;
-
-    public PostDto(Post post) {
-        this.id = post.getId();
-        this.createDate = post.getCreateDate();
-        this.modifyDate = post.getModifyDate();
-        this.subject = post.getTitle();
-        this.body = post.getContent();
+public record PostDto(
+        Long id,
+        LocalDateTime createDate,
+        LocalDateTime modifyDate,
+        String subject,
+        String body
+){
+    public PostDto(Post post){
+        this(
+                post.getId(),
+                post.getCreateDate(),
+                post.getModifyDate(),
+                post.getTitle(),
+                post.getContent()
+        );
     }
 }
