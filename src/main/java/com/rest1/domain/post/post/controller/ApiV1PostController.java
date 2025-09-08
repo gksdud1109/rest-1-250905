506f7690844df26b7939086c756e5d22fe510f4f
gksdud1109 <vectorh532@gmail.com>
2025-09-08 11:24:51 +0900

    feat(apiv1commnetcontroller, commentdto): impl api&dto for comment

--- src/main/java/com/rest1/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/rest1/domain/post/post/controller/ApiV1PostController.java
@@ -1,10 +1,12 @@
 package com.rest1.domain.post.post.controller;
 
 import com.rest1.domain.post.post.dto.PostDto;
+import com.rest1.domain.post.post.entity.Post;
 import com.rest1.domain.post.post.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +27,14 @@ public class ApiV1PostController {
         return postService.findAll().stream()
                 .map(PostDto::new)
                 .toList();
+    }
+
+    @GetMapping("/{id}")
+    @Transactional(readOnly = true)
+    public PostDto getItem(
+            @PathVariable Long id
+    ) {
+        Post post = postService.findById(id).get();
+        return new PostDto(post);
     }
 }
