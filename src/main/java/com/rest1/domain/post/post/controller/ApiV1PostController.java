e42b39273693cf9e7d49d310030856c00823f020
gksdud1109 <vectorh532@gmail.com>
2025-09-08 13:54:49 +0900

    feat(apiv1postcontroller): impl delete api

--- src/main/java/com/rest1/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/rest1/domain/post/post/controller/ApiV1PostController.java
@@ -3,6 +3,7 @@ package com.rest1.domain.post.post.controller;
 import com.rest1.domain.post.post.dto.PostDto;
 import com.rest1.domain.post.post.entity.Post;
 import com.rest1.domain.post.post.service.PostService;
+import com.rest1.global.rsData.RsData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -36,5 +37,19 @@ public class ApiV1PostController {
     ) {
         Post post = postService.findById(id).get();
         return new PostDto(post);
+    }
+
+    @GetMapping("/{id}/delete")
+    public RsData deleteItem(
+            @PathVariable Long id
+    ) {
+        Post post = postService.findById(id).get();
+        postService.delete(post);
+
+        return new RsData(
+                "204-1",
+                "%d번 게시물이 삭제되었습니다.".formatted(id),
+                new PostDto(post)
+        );
     }
 }
