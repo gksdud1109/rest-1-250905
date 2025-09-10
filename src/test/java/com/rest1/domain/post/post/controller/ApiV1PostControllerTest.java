edc246dc2b70c41928981db27dae5cf5c9729994
gksdud1109 <vectorh532@gmail.com>
2025-09-10 14:49:05 +0900

    test(postApi): get incase post not exit

--- src/test/java/com/rest1/domain/post/post/controller/ApiV1PostControllerTest.java
+++ src/test/java/com/rest1/domain/post/post/controller/ApiV1PostControllerTest.java
@@ -172,6 +172,7 @@ public class ApiV1PostControllerTest {
 
         assertThat(post).isNull();
     }
+
     @Test
     @DisplayName("글 단건 조회, 존재하지 않는 글")
     void t6() throws Exception {
@@ -192,5 +193,69 @@ public class ApiV1PostControllerTest {
         Post post = postRepository.findById(targetId).orElse(null);
 
         assertThat(post).isNull();
+    }
+
+    @Test
+    @DisplayName("글 작성, 제목이 입력되지 않은 경우")
+    void t7() throws Exception {
+        String title = "";
+        String content = "내용입니다.";
+
+
+        ResultActions resultActions = mvc
+                .perform(
+                        post("/api/v1/posts")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content("""
+                                        {
+                                            "title": "%s",
+                                            "content": "%s"
+                                        }
+                                        """.formatted(title, content)
+                                )
+                )
+                .andDo(print());
+
+        resultActions
+                .andExpect(handler().handlerType(ApiV1PostController.class))
+                .andExpect(handler().methodName("createItem"))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.resultCode").value("400-1"))
+                .andExpect(jsonPath("$.msg").value("""
+                        title-NotBlank-must not be blank
+                        title-Size-size must be between 2 and 10
+                        """.stripIndent().trim()));
+    }
+
+    @Test
+    @DisplayName("글 작성, 내용이 입력되지 않은 경우")
+    void t8() throws Exception {
+        String title = "제목입니다";
+        String content = "";
+
+
+        ResultActions resultActions = mvc
+                .perform(
+                        post("/api/v1/posts")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content("""
+                                        {
+                                            "title": "%s",
+                                            "content": "%s"
+                                        }
+                                        """.formatted(title, content)
+                                )
+                )
+                .andDo(print());
+
+        resultActions
+                .andExpect(handler().handlerType(ApiV1PostController.class))
+                .andExpect(handler().methodName("createItem"))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.resultCode").value("400-1"))
+                .andExpect(jsonPath("$.msg").value("""
+                        content-NotBlank-must not be blank
+                        content-Size-size must be between 2 and 100
+                        """.stripIndent().trim()));
     }
 }
